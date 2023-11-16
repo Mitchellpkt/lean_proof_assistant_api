6f87cfb30484bd5387decaa2e7068a1f713d2afe
Mitchell P. Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2023-11-15 22:21:59 -0500

    chore: start setup script

---
+++ install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Lean Theorem Prover
+sudo apt-get install -y lean
+
+# Install Python and Pip (If not already installed)
+sudo apt-get install -y python3 python3-pip
+
+# Install Flask for API development
+pip3 install Flask
