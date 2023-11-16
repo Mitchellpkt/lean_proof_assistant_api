53b8290d2ef93c2f70bbe4cb1326cc8109d66213
Mitchell P. Krawiec-Thayer <mitchellpkt+github@protonmail.com>
2023-11-15 22:27:48 -0500

    fix: lean install

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install Lean Theorem Prover
-sudo apt-get install -y lean
+# Super secure way to install lean /s
+wget -q https://raw.githubusercontent.com/leanprover-community/mathlib4/master/scripts/install_debian.sh && bash install_debian.sh ; rm -f install_debian.sh && source ~/.profile
 
 # Install Python and Pip (If not already installed)
 sudo apt-get install -y python3 python3-pip
